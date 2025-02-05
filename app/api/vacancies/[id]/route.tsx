66a748c17f44bd4bede5d6f0d3b2d61e9998b61c
Mathias Wakgari <104831041+mathiaswakgari@users.noreply.github.com>
2025-02-05 13:48:54 +0300

    feat: remove vacancies.

--- app/api/vacancies/[id]/route.tsx
+++
@@ -1,7 +0,0 @@
-import { authenticate } from "@/middlewares/auth";
-import { isAdmin } from "@/middlewares/isAdmin";
-import { getVacancy, editVacancy, deleteVacancy } from "../controllers/vacancy";
-
-export const GET = getVacancy;
-export const PATCH = authenticate(isAdmin(editVacancy));
-export const DELETE = authenticate(isAdmin(deleteVacancy));
--- app/api/vacancies/route.tsx
+++
@@ -1,6 +0,0 @@
-import { authenticate } from "@/middlewares/auth";
-import { isAdmin } from "@/middlewares/isAdmin";
-import { createVacancy, getVacancies } from "./controllers/vacancy";
-
-export const POST = authenticate(isAdmin(createVacancy));
-export const GET = getVacancies;
