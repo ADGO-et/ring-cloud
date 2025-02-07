3f525353fe20fdf4cdf160bbba019bc99e508225
Fraol7 <fraolmulugeta17@gmail.com>
2025-02-07 22:58:11 +0300

    bolg2

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,10 +4,16 @@ import SecondaryVlog from '@/components/blog-comp/SecondaryVlog'
 import Banner from '@/components/blog-comp/Banner'
 import EditorsPick from '@/components/blog-comp/EditorsPick'
 import NewsletterSection from '@/components/blog-comp/NewsletterSection'
+import Header from '@/components/blog-comp/(blog2)/Header'
+import BlogLayout from '@/components/blog-comp/(blog2)/BlogLayout'
+import YouMightLoveSection from '@/components/blog-comp/(blog2)/YouMightLoveSection'
 
 const page = () => {
   return (
     <div>
+      <YouMightLoveSection />
+      <Header />
+      <BlogLayout />
       <MajorBlog/>
       <SecondaryVlog/>
       <Banner />
