3a54bbf962b078741a00be556a624f1141dc1c5a
Melakeselam Yitbarek <Melakeselamyitbarek2012@gmail.com>
2025-02-28 13:19:23 +0300

    insert map

--- components/contact-us/MapComponent.tsx
+++ components/contact-us/MapComponent.tsx
@@ -1,31 +1,21 @@
 "use client";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
-
-const containerStyle = {
-  width: "100%",
-  height: "210px",
-};
 
 const MapComponent = () => {
-  const lat = 8.96363;
-  const lng = 38.83552;
+  const lat = 8.9661;
+  const lng = 38.8405;
 
-  const center = {
-    lat: lat,
-    lng: lng,
-  };
+  const mapSrc = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10000!2d${lng}!3d${lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2z${lat},${lng}!5e0!3m2!1sen!2sus!4v1614123456789!5m2!1sen!2sus`;
 
   return (
-    <LoadScript googleMapsApiKey="AIzaSyCz61eQinGa5Jd43DrRFQBDR3x0GzTyL4s">
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={18}
-        mapTypeId="satellite"
-      >
-        <Marker position={center} />
-      </GoogleMap>
-    </LoadScript>
+    <iframe
+      src={mapSrc}
+      width="400"
+      height="220"
+      style={{ border: 0 }}
+      allowFullScreen
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+    ></iframe>
   );
 };
 
